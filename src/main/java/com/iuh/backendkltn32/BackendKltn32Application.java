680fb7bc549f12e8fb6321466dadcb3a92f027c5
DangNguyen <ndang1336@gmail.com>
2023-04-15 12:08:00 +0700

    fixx issue

--- src/main/java/com/iuh/backendkltn32/BackendKltn32Application.java
+++ src/main/java/com/iuh/backendkltn32/BackendKltn32Application.java
@@ -1,5 +1,6 @@
 package com.iuh.backendkltn32;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
